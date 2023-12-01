b661f025e08b651d84becc21cab50605e508e068
Melvin Hedman <mehe2100@student.miun.se>
2023-12-01 13:42:10 +0100

    Banger2

--- 1/1.rb
+++ 1/1.rb
@@ -1,6 +1,5 @@
 value = 0
 File.readlines('./1/input.txt', chomp: true).each do |line|
-  new_num = line[/\d/] + line.reverse[/\d/]
-  value = value + Integer(new_num)
+  value = value + Integer(line[/\d/] + line.reverse[/\d/])
 end
 puts value
