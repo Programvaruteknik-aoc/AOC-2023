8c68018086034b1da7a2dbb39da07a1c40190e2f
Melvin Hedman <mehe2100@student.miun.se>
2023-12-01 13:36:24 +0100

    Optimization

--- 1/1.rb
+++ 1/1.rb
@@ -1,16 +1,12 @@
 def find_first_number(text)
   text[/\d/]
 end
-
 def find_last_number(text)
   text.reverse[/\d/]
 end
-
 value = 0
-
 File.readlines('./1/input.txt', chomp: true).each do |line|
   new_num = find_first_number(line) + find_last_number(line)
   value = value + Integer(new_num)
 end
-
 puts value
