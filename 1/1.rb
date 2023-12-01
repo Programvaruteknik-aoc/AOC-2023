52a6d871a1a6aee4392540dca626987d8046ff2e
Melvin Hedman <mehe2100@student.miun.se>
2023-12-01 13:39:50 +0100

    Banger

--- 1/1.rb
+++ 1/1.rb
@@ -1,12 +1,6 @@
-def find_first_number(text)
-  text[/\d/]
-end
-def find_last_number(text)
-  text.reverse[/\d/]
-end
 value = 0
 File.readlines('./1/input.txt', chomp: true).each do |line|
-  new_num = find_first_number(line) + find_last_number(line)
+  new_num = line[/\d/] + line.reverse[/\d/]
   value = value + Integer(new_num)
 end
 puts value
