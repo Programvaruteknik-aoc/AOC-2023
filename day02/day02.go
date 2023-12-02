5b3b04dcc48268ab13ab88b2084891be775691b9
fnord <fino2101@student.miun.se>
2023-12-02 21:19:02 +0100

    Day 2 completed

--- day02/day02.go
+++ day02/day02.go
@@ -16,37 +16,58 @@ func RunDay02() {
 	fileScanner := bufio.NewScanner(file)
 	fileScanner.Split(bufio.ScanLines)
 	var totalIdSum = 0
+	var powerOf = 0
 	for fileScanner.Scan() {
-		totalIdSum += readCubes(fileScanner.Text())
+		sum, power := readCubes(fileScanner.Text())
+		totalIdSum += sum
+		powerOf += power
 	}
 	fmt.Println("Total sum of ids:", totalIdSum)
+	fmt.Println("Total power of:", powerOf)
 }
 
-func readCubes(str string) int {
+func readCubes(str string) (int, int) {
 	separateGameId := strings.Split(str, ":")
 	parts := strings.Split(separateGameId[1], ";")
 	var approved = true
 	var toAdd = 2
 	var times = 1
+	var maxGreen = 0
+	var maxRed = 0
+	var maxBlue = 0
+	var currentNumb = 0
 
 	for j := 0; j < len(parts); j++ {
 		str = strings.TrimSpace(parts[j])
 		var colorMap = map[string]int{"Red": 0, "Green": 0, "Blue": 0}
 		for i := 0; i < len(str); i++ {
+			currentNumb = 0
 			if unicode.IsDigit(rune(str[i])) {
 				toAdd = 2
 				times = 1
 				if unicode.IsDigit(rune(str[i+1])) {
 					toAdd = 3
 					times = 10
+					currentNumb = int(str[i]-'0')*times + int(str[i+1]-'0')
+				} else {
+					currentNumb += int(str[i]) - '0'
 				}
 
 				if string(str[i+toAdd]) == "r" {
 					colorMap["Red"] += (int(str[i]) - '0') * times
+					if maxRed < currentNumb {
+						maxRed = currentNumb
+					}
 				} else if string(str[i+toAdd]) == "b" {
 					colorMap["Blue"] += (int(str[i]) - '0') * times
+					if maxBlue < currentNumb {
+						maxBlue = currentNumb
+					}
 				} else if string(str[i+toAdd]) == "g" {
 					colorMap["Green"] += (int(str[i]) - '0') * times
+					if maxGreen < currentNumb {
+						maxGreen = currentNumb
+					}
 				}
 			}
 
@@ -59,11 +80,12 @@ func readCubes(str string) int {
 	reg := regexp.MustCompile("\\b+[0-9]+\\b")
 	matches := reg.FindStringSubmatch(separateGameId[0])
 	gameId, _ := strconv.Atoi(matches[0])
+	power := (maxRed * maxGreen) * maxBlue
 
 	if approved {
-		return gameId
+		return gameId, power
 	}
-	return 0
+	return 0, power
 }
 
 func controlMap(colorMap map[string]int) bool {
