476a3ea6f863f2b2f42f8a918b60144b39a94a81
rubenmadsen <ruben@rubenmadsen.com>
2023-12-03 20:11:10 +0100

    day 3 1 2

--- src/day3p2.rs
+++ src/day3p2.rs
@@ -1,15 +1,16 @@
+use std::collections::{HashMap, HashSet};
 use std::sync::mpsc::sync_channel;
 use crate::day1_1::is_digit;
 use crate::helper::Data;
 
 fn validate_cell(pos:(i32,i32), xlimit:i32, ylimit:i32) -> Option<(i32,i32)> {
     if pos.0 >= 0 && pos.1 >= 0 && pos.0 < xlimit as i32 && pos.1 < ylimit as i32 {
-         return Some(pos);
-     }
+        return Some(pos);
+    }
     None
 }
 
-fn neighbours(pos: (i32, i32), xlimit: i32, ylimit:i32) -> Vec<(i32, i32)> {
+pub fn neighbours(pos: (i32, i32), xlimit: i32, ylimit:i32) -> Vec<(i32, i32)> {
     let mut nb: Vec<(i32, i32)> = Vec::new();
     nb.push((pos.0 - 1, pos.1 - 1));
     nb.push((pos.0 - 1, pos.1));
@@ -27,37 +28,48 @@ fn neighbours(pos: (i32, i32), xlimit: i32, ylimit:i32) -> Vec<(i32, i32)> {
         .collect()
 }
 
-    fn who_dis(data:&Data) -> String {
-        let mut symbols:String = String::from("");
-        data.input.lines().for_each(|line|{
-            line.chars().enumerate().for_each(|c|{
-                if is_digit(c.1) || c.1 == '.' {
+fn who_dis(data:&Data) -> String {
+    let mut symbols:String = String::from("");
+    data.input.lines().for_each(|line|{
+        line.chars().enumerate().for_each(|c|{
+            if is_digit(c.1) || c.1 == '.' {
 
+            }
+            else {
+                if !symbols.contains(c.1){
+                    symbols = format!("{}{}",symbols,c.1);
                 }
-                else {
-                    if !symbols.contains(c.1){
-                        symbols = format!("{}{}",symbols,c.1);
-                    }
-                }
-            })
-        });
-        symbols
-    }
+            }
+        })
+    });
+    symbols
+}
 
 pub fn start(){
-
+    let mut gears:HashMap<(u32,u32),Vec<u32>> = HashMap::new();
     let data : Data = Data::new(3,1);
     let symbols = who_dis(&data);
-    //let symbols = String::from("*#+$");
     let mut sum:u32 = 0;
     let mut line_num = 0;
     let mut lines:Vec<&str> = data.input.lines().collect();
     let lines_count = lines.len() as i32;
 
+        let mut ln = 0;
+    lines.iter().for_each(|line|{
+        line.chars().enumerate().for_each(|ch|{
+             if ch.1 == '*'{
+                 let gear = (ch.0 as u32, ln);
+                 gears.insert(gear,Vec::new());
+             }
+        });
+        ln += 1;
+    });
+    // gears.iter().for_each(|g|println!("Locations: {},{}\n",g.0.0,g.0.1));
     let mut has_pn = false;
     let mut word:String = String::from("");
+    let mut con:HashSet<(u32,u32)> = HashSet::new();
 
-    data.input.lines().for_each(|line|{
+    lines.iter().for_each(|line|{
         line.chars().enumerate().for_each(|ch|{
             let c = ch.1;
             let i = ch.0;
@@ -76,6 +88,7 @@ pub fn start(){
                         word = String::from("");
                         word = format!("{}{}", word, c);
                         has_pn = false;
+                        con.clear();
                     }
                     else if is_digit(prev.unwrap()){
                         word = format!("{}{}", word, c);
@@ -84,23 +97,25 @@ pub fn start(){
                 else if prev.is_none(){
                     word = String::from("");
                     word = format!("{}{}", word, c);
+                    con.clear();
                     has_pn = false;
 
                 }
                 // check neighbours
                 let coord = (i as i32,line_num);
                 let neighbours:Vec<(i32,i32)>  = neighbours(coord,line.len() as i32,lines_count);
-                print!("Neighbours: [{}]",neighbours.len());
+                // print!("Neighbours: [{}]",neighbours.len());
                 neighbours.iter().for_each(|cell|{
                     let ln:&str = lines.get(cell.1 as usize).unwrap();
                     let val:char = ln.chars().enumerate().nth(cell.0 as usize).unwrap().1;
-                    if symbols.contains(val){
-                        has_pn = true;
+                    if val == '*'{
+                        //
+                        let ncell = (cell.0 as u32,cell.1 as u32);
+                        con.insert(ncell);
+                        //
                     }
-                    print!("{}",val);
-
                 });
-                println!();
+                // println!();
 
                 let mut calc = false;
                 if next.is_some() {
@@ -114,11 +129,22 @@ pub fn start(){
                 if calc{
                     // has_pn = hasPart;
                     let v = word.parse::<u32>().unwrap();
-                    if has_pn {
-                        sum += v;
-                    }
-                    println!("[{}] {}", v, has_pn);
-                    println!();
+
+                    sum += v;
+                    con.iter().for_each(|acon| {
+                        let mut g = gears.get_mut(acon);
+                        match g {
+                            None => {
+                            }
+                            Some(g) => {
+                                g.push(v)
+                            }
+                        }
+                    });
+                    con.clear();
+
+                    // println!("[{}] {}", v, has_pn);
+                    // println!();
                 }
             }
             else{
@@ -128,6 +154,12 @@ pub fn start(){
         });
         line_num += 1;
     });
-    println!("SYMBOLS:[{}]",symbols);
+    sum = 0;
+    gears.iter().for_each(|gear|{
+        if gear.1.len() == 2{
+            let bisum = gear.1.get(0).unwrap() * gear.1.get(1).unwrap();
+            sum += bisum;
+        }
+    });
     println!("SUM:{}",sum);
 }
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ mod day2_1;
 
 mod day2p2;
 mod day3p1;
+mod day3p2;
 
 fn main() {
     //day1_1::start();
     //day1_2::start();
     //day2_1::start();
     //day2p2::start();
-    day3p1::start();
+    //day3p1::start();
+    day3p2::start();
 }
