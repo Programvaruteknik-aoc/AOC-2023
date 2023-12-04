264ab0db1134f6a9eca2b3f61a172053b10ec290
Samuel Thand <sath2102@student.miun.se>
2023-12-04 17:06:18 +0100

    Did day2 part2

--- lib/day2.ex
+++ lib/day2.ex
@@ -18,42 +18,59 @@ defmodule Day2 do
   end
 
   @doc """
-  Solves part2 by
+  Solves part2 by doing the same, but extracting the minimum required cubes for each game,
+  multiplies them, and sums them.
   """
   def part2(input) do
-    :not_implemented
+    stream_lines(input)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&parse_game/1)
+    |> Enum.map(&minimum_cubes/1)
+    |> Enum.map(&power_of_set/1)
+    |> Enum.sum()
   end
 
   def parse_game(line) do
-    [game_info | moves_info] = String.split(line, ": ")
+    [game_info | sets_info] = String.split(line, ": ")
     id = game_info
           |> String.split(" ")
           |> Enum.at(1)
           |> Integer.parse()
           |> elem(0)
-    moves = moves_info
+    sets = sets_info
             |> List.first()
             |> String.split("; ")
-            |> Enum.map(&parse_move/1)
-    %{id: id, moves: moves}
+            |> Enum.map(&parse_set/1)
+    %{id: id, sets: sets}
   end
 
-  def parse_move(move) do
-    move
+  def parse_set(set) do
+    set
     |> String.split(", ")
-    |> Enum.map(&parse_cube/1)
+    |> Enum.map(&parse_cubes/1)
   end
 
-  def parse_cube(cube) do
-    [count, color] = String.split(cube, " ")
+  def parse_cubes(cubes) do
+    [count, color] = String.split(cubes, " ")
     {String.to_atom(color), String.to_integer(count)}
   end
 
-  def is_possible(%{id: _, moves: moves}) do
-    Enum.all?(moves, fn move ->
-      Enum.all?(move, fn {color, count} ->
+  def is_possible(%{id: _, sets: sets}) do
+    Enum.all?(sets, fn set ->
+      Enum.all?(set, fn {color, count} ->
         count <= Map.get(@inventory, color)
       end)
     end)
   end
+
+  def minimum_cubes(%{sets: sets}) do
+    Enum.reduce(sets, %{red: 0, green: 0, blue: 0}, fn set, accumulator ->
+      Enum.reduce(set, accumulator, fn {color, count}, accumulator ->
+        Map.update(accumulator, color, count, &max(count, &1))
+      end)
+    end)
+  end
+
+  def power_of_set(%{red: r, green: g, blue: b}), do: r * g * b
+
 end
--- test/day2_test.exs
+++ test/day2_test.exs
@@ -11,22 +11,42 @@ defmodule Day2Test do
 
   test "should return parsed game" do
     assert parse_game("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green") ==
-      %{id: 1, moves: [
+      %{id: 1, sets: [
         [{:blue, 3}, {:red, 4}],
         [{:red, 1}, {:green, 2}, {:blue, 6}],
         [{:green, 2}]
       ]}
   end
 
-  test "should return parsed move" do
-    assert parse_move("3 blue, 4 red") == [{:blue, 3}, {:red, 4}]
+  test "should return parsed set" do
+    assert parse_set("3 blue, 4 red") == [{:blue, 3}, {:red, 4}]
   end
 
   test "should return parsed cube" do
-    assert parse_cube("3 blue") == {:blue, 3}
+    assert parse_cubes("3 blue") == {:blue, 3}
+  end
+
+  test "should return if a move is possible" do
+    assert is_possible(%{id: 1, sets: [
+      [{:blue, 3}, {:red, 4}],
+      [{:red, 1}, {:green, 2}, {:blue, 6}],
+      [{:green, 2}]
+    ]}) == true
   end
 
 
   @simpledoc "Part 2"
+
+  test "should return minimum cubes" do
+    assert minimum_cubes(%{id: 1, sets: [
+      [{:blue, 3}, {:red, 4}],
+      [{:red, 1}, {:green, 2}, {:blue, 6}],
+      [{:green, 2}]
+    ]}) == %{red: 4, green: 2, blue: 6}
+  end
+
+  test "should return power of set" do
+    assert power_of_set(%{red: 4, green: 2, blue: 6}) == 48
+  end
 
 end
