eb15cf73a92b5efb265f824f063abe74a3fea61f
rubenmadsen <ruben@rubenmadsen.com>
2023-12-08 12:46:06 +0100

    slask

--- src/day7p2.rs
+++ src/day7p2.rs
@@ -1,6 +1,14 @@
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use crate::helper::Data;
+const FIVE_OF_A_KIND: u64 = 7;
+const FOUR_OF_A_KIND: u64 = 6;
+const FULL_HOUSE: u64 = 5;
+const THREE_OF_A_KIND: u64 = 4;
+const TWO_PAIR: u64 = 3;
+const ONE_PAIR: u64 = 2;
+const HIGH_CARD: u64 = 1;
+
 
 fn find_value_for_card(card:char) -> u32{
     let values:Vec<char> = vec!['X','J','2','3','4','5','6','7','8','9','T','J','Q','K','A'];
@@ -13,8 +21,8 @@ fn find_value_by_card(hand1:&str, hand2:&str) -> (u32,u32){
     let mut index = 0;
     let mut a = hand1.chars().nth(index).unwrap();
     let mut b = hand2.chars().nth(index).unwrap();
-    println!("{}<->{}",hand1,hand2);
-    println!("***********");
+    //println!("{}<->{}",hand1,hand2);
+    //println!("***********");
     while a == b {
         a = hand1.chars().nth(index).unwrap();
         b = hand2.chars().nth(index).unwrap();
@@ -50,31 +58,32 @@ fn find_rank(hand:&str) -> (&str, u32,u64) {
     match highest {
         5 => {
             // Five of a kind
-            rank = 7;
+            rank = FIVE_OF_A_KIND;
         }
         4 => {
             // Four of a kind
-            rank = 6;
-            if has_j == 1 {
-                rank = 7;
+            rank = FOUR_OF_A_KIND;
+            if has_j > 0{
+                rank = FIVE_OF_A_KIND;
             }
         }
         3 => {
             if  next_highest == 2{
                 // Full house
-                rank = 5;
-                if has_j == 1 {
-                    rank = 6;
-                }
-                else if has_j == 2{
-                    rank = 7;
+                rank = FULL_HOUSE;
+
+                if has_j > 0{
+                    rank = FIVE_OF_A_KIND;
                 }
             }
-            else{
-                // Three of a kind
-                rank = 4;
-                if has_j > 0{
-                    rank = 6;
+            else if next_highest == 1{
+                let next_next_pair = pairs.get(2).unwrap().clone();
+                let next_highest = *next_highest_pair.1;
+                if *next_highest_pair.0 == 'J' || *next_next_pair.0 == 'J'{
+                    rank = FOUR_OF_A_KIND;
+                }
+                else {
+                    rank = THREE_OF_A_KIND;
                 }
             }
 
@@ -82,28 +91,26 @@ fn find_rank(hand:&str) -> (&str, u32,u64) {
         2 => {
             if next_highest == 2{
                 // Two pairs
-                rank = 3;
+                rank = TWO_PAIR;
                 if has_j == 1 {
-                    rank = 5;
+                    rank = FULL_HOUSE;
                 }
                 else if has_j == 2{
-                    rank = 6;
+                    rank = FOUR_OF_A_KIND;
                 }
             }
             else{
                 // One Pair
-                rank = 2;
-                if has_j == 1{
-                    rank = 4
+                rank = ONE_PAIR;
+                if has_j == 1 || has_j == 2{
+                    rank = THREE_OF_A_KIND
                 }
-
             }
-
         }
         1 => {
-            rank = 1;
+            rank = HIGH_CARD;
             if has_j == 1 {
-                rank = 2;
+                rank = ONE_PAIR;
             }
         }
         _ => { rank = 0;}
@@ -115,7 +122,7 @@ pub fn start(){
     let data:Data = Data::new(7,2);
     let mut hands:HashMap<&str,&str> = HashMap::new();
     let mut ranks:HashMap<&str,u32> = HashMap::new();
-    data.example.lines().for_each(|line|{
+    data.input.lines().for_each(|line|{
         let kv = line.split_once(" ").unwrap();
         hands.insert(kv.0,kv.1);
     });
@@ -132,7 +139,7 @@ pub fn start(){
     for index in 1..highest+1 {
         let start = ranked_hands.iter().position(|&(_, v, _)| v == index).unwrap_or(0);
         let end = ranked_hands.iter().rposition(|&(_, v, _)| v == index).map_or(start, |i| i + 1);
-        println!("Index:{} start:{} end:{}",index,start,end);
+        //println!("Index:{} start:{} end:{}",index,start,end);
         ranked_hands[start..end].sort_by(|a, b|{
             let values = find_value_by_card((*a).0,(*b).0);
             let a_val = values.0;
@@ -154,39 +161,17 @@ pub fn start(){
     let mut counter = 1;
     let mut info:String = String::from("");
     for (key, value, bid) in ranked_reverse{
-        println!("Hand:{}: Rank:{} Bid:{} Preorder:{}", key, counter,bid,value);
+        //println!("Hand:{}: Rank:{} Bid:{} Preorder:{}", key, counter,bid,value);
         info = format!("{} {} * {} + ",info ,bid,counter);
         sum += counter*bid;
         counter += 1;
     }
-    println!("{}",info);
+    //println!("{}",info);
     println!("Sum:{}",sum);
 }
 // 765 * 1 + 220 * 2 + 28 * 3 + 684 * 4 + 483 * 5
 // 765 * 1 + 28 * 2 + 220 * 3 + 483 * 4 + 684 * 5 +
 /*
-32T3K
-KTJJT
-KK677
-T55J5
-QQQJA
-
- Hand:32T3K: Rank:1 Bid:765 Preorder:2
-Hand:KTJJT: Rank:2 Bid:220 Preorder:3
-Hand:KK677: Rank:3 Bid:28 Preorder:3
-Hand:QQQJA: Rank:4 Bid:483 Preorder:4
-Hand:T55J5: Rank:5 Bid:684 Preorder:4
-
-Hand:32T3K: Rank:1 Bid:765 Preorder:2
-Hand:KTJJT: Rank:2 Bid:220 Preorder:3
-Hand:KK677: Rank:3 Bid:28 Preorder:3
-Hand:T55J5: Rank:4 Bid:684 Preorder:4
-Hand:QQQJA: Rank:5 Bid:483 Preorder:4
-low 253269737
-low 253271833
-253269996
-253264768
-253273054
-low 253293482
-    253301037
+253273179
+253499763
  */
