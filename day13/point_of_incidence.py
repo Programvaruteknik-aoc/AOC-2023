5a77fd696dd2449ca03e044c2e5da390c69bbcd3
Tobias Hansson <tobiashansson93@gmail.com>
2023-12-13 10:26:49 +0100

    Prep day13

---
+++ day13/point_of_incidence.py
@@ -0,0 +1,15 @@
+from utils.util import get_input
+
+def main() -> None:
+    file_input = get_input("day12/input.txt")
+    print(f"Part one: {part_one(file_input)}")
+    print(f"Part two: {part_two(file_input)}")
+
+def part_one(input: list[str]) -> int:
+    return 0
+
+def part_two(input:list[str]) -> int:
+    return 0
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_day13.py
@@ -0,0 +1,19 @@
+import unittest
+from day13 import point_of_incidence
+
+test_input_1 = """\
+"""
+
+class TestDay13(unittest.TestCase):
+    def test_part_one(self):
+        expected_result = -1
+        result:int = point_of_incidence.part_one(test_input_1.split("\n"))
+        self.assertEqual(result, expected_result)
+
+    def test_part_two(self):
+        expected_result = -1
+        result:int = point_of_incidence.part_two(test_input_1.split("\n"))
+        self.assertEqual(result, expected_result)
+
+if __name__ == "__main__":
+    unittest.main()
